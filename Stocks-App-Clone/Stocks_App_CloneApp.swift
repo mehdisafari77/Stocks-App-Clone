aee6ba51f7bfa1f9254e4342374be6209ae1fd1c
Mehdi Safari <75599021+mehdisafari77@users.noreply.github.com>
2022-07-27 21:06:27 -0700

    Initial Commit

---
+++ Stocks-App-Clone/Stocks_App_CloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  Stocks_App_CloneApp.swift
+//  Stocks-App-Clone
+//
+//  Created by Mehdi MMS on 27/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct Stocks_App_CloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
