5c2e424c238685be93f668733a5b1cb83c30e1ce
Mehdi Safari <75599021+mehdisafari77@users.noreply.github.com>
2022-08-03 09:18:19 -0700

    edits in API file

--- Stocks-App-Clone/Features/Quotes/Managers/API.swift
+++ Stocks-App-Clone/Features/Quotes/Managers/API.swift
@@ -9,4 +9,24 @@ import Foundation
 
 struct API {
     
+    static var baseUrl: String {
+        return "https://alphavantage.co/query?"
+    }
+    
+    static func symbolSearchUrl(for searchKey: String) -> String {
+        
+    }
+    
+    private static func urlBy(symbol: SymbolFunction, searchKey: String) -> String {
+        switch symbol {
+        case .search:
+            return "\(baseUrl)function"
+        default:
+            //
+        }
+    }
+    enum SymbolFunction: String {
+        case search = "SYMBOL_SEARCH"
+        case quote = "GLOBAL_QUOTE"
+    }
 }
