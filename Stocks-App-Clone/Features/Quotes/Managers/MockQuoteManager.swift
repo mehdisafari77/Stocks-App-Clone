4193b11d86ac36ec64dec7edcfc30bf4080c7e53
Mehdi Safari <75599021+mehdisafari77@users.noreply.github.com>
2022-08-04 10:06:25 -0700

    done with stock quote manager

--- Stocks-App-Clone/Features/Quotes/Managers/MockQuoteManager.swift
+++ Stocks-App-Clone/Features/Quotes/Managers/MockQuoteManager.swift
@@ -11,7 +11,7 @@ final class MockQuoteManager: QuoteManagerProtocol, ObservableObject {
     
     @Published var quotes: [Quote] = []
     
-    func download(stock: [String], completion: @escaping (Result<[Quote], NetworkError>) -> Void) {
+    func download(stocks: [String], completion: @escaping (Result<[Quote], NetworkError>) -> Void) {
         let aapl = Quote(symbol: "AAPL", open: "135.90", high: "136.39", low: "133.77", price: "135.13", change: "-0.26", changePercent: "-0.19%")
         let goog = Quote(symbol: "GOOG", open: "2099.51", high: "2102.03", low: "2077.32", price: "2095.89", change: "0.51", changePercent: "0.024%")
         
--- Stocks-App-Clone/Features/Quotes/Managers/QuoteManagerProtocol.swift
+++ Stocks-App-Clone/Features/Quotes/Managers/QuoteManagerProtocol.swift
@@ -9,5 +9,5 @@ import Foundation
 
 protocol QuoteManagerProtocol {
     var quotes: [Quote] { get set }
-    func download(stock: [String], completion: @escaping (Result<[Quote], NetworkError>) -> Void)
+    func download(stocks: [String], completion: @escaping (Result<[Quote], NetworkError>) -> Void)
 }
